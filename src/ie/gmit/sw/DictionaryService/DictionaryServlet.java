b17804a921e256815dcf4e8e0c9cb82b5fcb5171
Ryan Gordon <ryangordon210@gmail.com>
2017-12-21 23:35:48 +0000

    Attempting response in post

--- src/ie/gmit/sw/DictionaryService/DictionaryServlet.java
+++ src/ie/gmit/sw/DictionaryService/DictionaryServlet.java
@@ -27,14 +27,24 @@ public class DictionaryServlet extends HttpServlet {
 	 */
 	private String environmentalVariable = null; //Demo purposes only. Rename this variable to something more appropriate
 	private static long jobNumber = 0;
-	
+	private InQueue r;
+	private String result;
     protected void doGet(HttpServletRequest request, HttpServletResponse response, String res) throws ServletException, IOException {
-    				
+    			
 	}
 
     @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    		
+    		r = new InQueue(jobNumber,request.getParameter("word"));	
+		try {
+			Thread.sleep(5000);					// simulate a wait...?
+			String res = r.getResponse();	
+			
+			doGet(request, response, res);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}// try catch
 	}
 
 }
